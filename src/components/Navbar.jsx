fc7d39ccea4e12197a21768debe1a9a93de1d698
ShidiKAY <47364468+ShidiKAY@users.noreply.github.com>
2025-06-25 13:22:31 +0200

    Update navbar for esponsibe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,12 @@ import { useEffect, useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { SiMalt } from "react-icons/si";
-import { Link, useLocation } from "react-router-dom";
+// import { Link, useLocation } from "react-router-dom";
 import { Link as ScrollLink, Events } from "react-scroll"; // Import ScrollLink and animateScroll
 import { useTranslation } from "react-i18next";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { pathname } = useLocation(); // Destructure pathname from useLocation
   const [isScrolled, setIsScrolled] = useState(false);
   const [activeLink, setActiveLink] = useState("home"); // State to track active link
   const sections = ["tohome", "toabout", "toprojects", "toskills"];
@@ -227,109 +226,106 @@ const Navbar = () => {
               </div>
               <div>
                 <div className="flex flex-col space-y-4 pt-4">
-                  <h1
-                    className={
-                      pathname == "/"
-                        ? "text-blue-500"
-                        : "cursor-pointer hover:text-blue-500"
-                    }
-                  >
-                    <Link id="home" to="/" onClick={handleMobileNav}>
-                      Home
-                    </Link>
-                  </h1>
-                  <h1
-                    className={
-                      pathname == "/about"
-                        ? "text-blue-500"
-                        : "cursor-pointer hover:text-blue-500"
-                    }
-                  >
-                    <Link id="about" to="/about" onClick={handleMobileNav}>
-                      About
-                    </Link>
-                  </h1>
-                  <h1
-                    className={
-                      pathname == "/skills"
-                        ? "text-blue-500"
-                        : "cursor-pointer hover:text-blue-500"
-                    }
-                  >
-                    <Link id="skills" to="/skills" onClick={handleMobileNav}>
-                      Skills
-                    </Link>
-                  </h1>
-                  <h1
-                    className={
-                      pathname == "/projects"
-                        ? "text-blue-500"
-                        : "cursor-pointer hover:text-blue-500"
-                    }
-                  >
-                    <Link
-                      id="projects"
-                      to="/projects"
+                  <h1 className={handleActiveLink("tohome")}>
+                    <ScrollLink
+                      to="tohome"
+                      smooth={true}
+                      duration={500}
+                      className="cursor-pointer"
                       onClick={handleMobileNav}
+                      onSetActive={() => setActiveLink("tohome")}
                     >
-                      Projects
-                    </Link>
+                      {t("common.home")}
+                    </ScrollLink>
                   </h1>
-                  <h1
-                    className={
-                      pathname == "/blog"
-                        ? "text-blue-500"
-                        : "cursor-pointer hover:text-blue-500"
-                    }
-                  >
-                    <Link id="blog" to="/blog" onClick={handleMobileNav}>
-                      Blog
-                    </Link>
+                  <h1 className={handleActiveLink("toabout")}>
+                    <ScrollLink
+                      to="toabout"
+                      smooth={true}
+                      duration={500}
+                      className="cursor-pointer"
+                      onClick={handleMobileNav}
+                      onSetActive={() => setActiveLink("toabout")}
+                    >
+                      {t("common.about")}
+                    </ScrollLink>
                   </h1>
-
-                  <ul className="flex space-x-2">
-                    <li
-                      className="inline-flex items-center cursor-pointer"
-                      onClick={() => changeLanguage("fr")}
+                  <h1 className={handleActiveLink("toskills")}>
+                    <ScrollLink
+                      to="toskills"
+                      smooth={true}
+                      duration={500}
+                      className="cursor-pointer"
+                      onClick={handleMobileNav}
+                      onSetActive={() => setActiveLink("toskills")}
                     >
-                      <span
-                        className="i-flagpack-fr w-6 mb-1 mt-1 mr-1"
-                        title="French"
-                      ></span>
-                      French
-                    </li>
-                    <li
-                      className="inline-flex items-center cursor-pointer"
-                      onClick={() => changeLanguage("en")}
+                      {t("common.skills")}
+                    </ScrollLink>
+                  </h1>
+                  <h1 className={handleActiveLink("toprojects")}>
+                    <ScrollLink
+                      to="toprojects"
+                      smooth={true}
+                      duration={500}
+                      className="cursor-pointer"
+                      onClick={handleMobileNav}
+                      onSetActive={() => setActiveLink("toprojects")}
                     >
-                      <span
-                        className="i-flagpack-gb-ukm w-6 mt-px mr-1"
-                        title="English"
-                      ></span>{" "}
-                      English
-                    </li>
-                  </ul>
+                      {t("common.projects")}
+                    </ScrollLink>
+                  </h1>
+                  <div className="language-switcher mt-1.5">
+                    <ul className="flex space-x-2">
+                      <li
+                        className="inline-flex items-center cursor-pointer"
+                        onClick={() => changeLanguage("fr")}
+                      >
+                        <span
+                          className="i-flagpack-fr w-8 mb-1 mt-1"
+                          title="French"
+                        ></span>
+                        <span className="hidden lg:block">French</span>
+                      </li>
+                      <li
+                        className="inline-flex items-center cursor-pointer"
+                        onClick={() => changeLanguage("en")}
+                      >
+                        <span
+                          className="i-flagpack-gb-ukm w-8 mt-px"
+                          title="English"
+                        ></span>{" "}
+                        <span className="hidden lg:block">English</span>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-                <div className="space-x-4 pt-20">
-                  <div className="flex space-x-4">
-                    <div className="rounded shadow-lg shadow-gray-400 p-3 cursor-pointer">
-                      <a
-                        href="https://github.com/prasad-chavan1"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <BsGithub size="2rem" />
-                      </a>
-                    </div>
-                    <div className="rounded shadow-lg shadow-gray-400 p-3 cursor-pointer">
-                      <a
-                        href="https://www.linkedin.com/in/prasad-chavan2003/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <BsLinkedin size="2rem" />
-                      </a>
-                    </div>
+                <div className="flex space-x-4 pt-20">
+                  <div>
+                    <a
+                      href="https://github.com/ShidiKAY"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <BsGithub size="2rem" />
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://www.linkedin.com/in/kamal-ait-yous-90a6a3178/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <BsLinkedin size="2rem" />
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://www.malt.fr/profile/kamalaityous"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <SiMalt size="2rem" />
+                    </a>
                   </div>
                 </div>
               </div>
